042731da61b4aaf9e83de46257d50f2a3bfd855d
parince <konrad.tupta@gmail.com>
2023-10-20 21:47:12 +0200

    Exercise 3 from week 2

---
+++ week02/exercise03.c
@@ -0,0 +1,52 @@
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+#ifndef _BV
+    #define _BV(bit) (1<<(bit))
+#endif
+
+int main(void)
+{
+    DDRB |= _BV(PB0)| _BV(PB1)| _BV(PB2);
+    DDRC &= ~_BV(PC0);
+    DDRC &= ~_BV(PC1);
+    PORTC |= _BV(PC0) | _BV(PC1);
+
+    while (1)
+    {
+        if (!(PINC & _BV(PC0)))
+        {
+            PORTB |= _BV(PB0) | _BV(PB1);
+            PORTB &= ~_BV(PB2);
+            _delay_ms(500);
+
+            PORTB |= _BV(PB0) | _BV(PB2);
+            PORTB &= ~_BV(PB1);
+            _delay_ms(500);
+
+            PORTB |= _BV(PB1) | _BV(PB2);
+            PORTB &= ~_BV(PB0);
+            _delay_ms(500);
+        }
+        else if (!(PINC & _BV(PC1)))
+        {
+            PORTB |= _BV(PB1) | _BV(PB2);
+            PORTB &= ~_BV(PB0);
+            _delay_ms(500);
+
+            PORTB |= _BV(PB0) | _BV(PB2);
+            PORTB &= ~_BV(PB1);
+            _delay_ms(500);
+
+            PORTB |= _BV(PB0) | _BV(PB1);
+            PORTB &= ~_BV(PB2);
+            _delay_ms(500);
+        }
+        else
+        {
+            PORTB |= _BV(PB0)| _BV(PB1)| _BV(PB2);
+        }
+    }
+    return 0;
+}
